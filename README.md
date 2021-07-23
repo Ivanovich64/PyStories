7f90aba33970dbcb1fae4d1432449e3dca397396
Ivanovich64 <ivanovich6v4@gmail.com>
2021-07-23 10:39:44 -0600

    Change in paradigm of tracing the story and adding some basic generalities on the README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PyStories
 A series of interactive stories that change as you take decisions or make up your own. Small project.
+
+# Game Mechanics
+## Decisions
+There are some decisions throughout the game that might change from a small portion to the entire development of the story. Most decisions affect only the path of the current chapter, but there are some key paths that lead to a significant change in the whole direction of the story.
+
+## Zentrum
+The most important characteristics, values and decisions of the character compose the Zentrum. It describes the core of the character in the game, and is the main source for presenting dialogue, path or action options. That way the game learns a little bit about the character and shapes the path for the player.
+From choosing a special name all the way to whether picking up a seemingly unimportant item can lead to a major difference in the gamplay experience. 
--- SmallAdventure/main.py
+++ SmallAdventure/main.py
@@ -1,10 +1,23 @@
-def intro():
+#Creado por: Ivanovich6v4
+#Historia por: Javier Fernandez
+##Version de Python: 3.9.5
+#Fecha de creacion: 21 de Julio 2021
+import time
+
+def intro(zentrum):
     print("*·*·*·*·*·*·*·*·*·*·*·*·*·*·*·*·*\n  Welcome to <Small Adventure>!\n  Enjoy your own small story.\n*·*·*·*·*·*·*·*·*·*·*·*·*·*·*·*·*\n ")
     name = input("Please enter your \"name\": \n  > ")
-    return name
+    zentrum['Name'] = name
+    zentrum['SpecialName'] = 0
 
-def prelude(name):
-    story = [name]
+    if name.lower() == "Baron" or name.lower() == "Fidelio" or name.lower() == "":
+        zentrum['SpecialName']=1
+    
+
+    print("\n\n\n   ---This is now the story of "+str(zentrum['Name']+"---"))
+    return zentrum
+
+def prelude(zentrum):
     
     print("\n\n It is a really dark night outside.\n It's already past midnight and you decide to go for a drive.\n Driving calms you and helps you stop thinking about your struggles and issues. You can feel the wind brushing your hair as you accelerate on the road to the country.\n Suddenly, the car loses strength and begins to slow down until completely stopping at the side of the road.\n You step out of the car and feel the mud under your shoes sticking as you walk across the road.\n   What now?\n")
     
@@ -13,12 +26,28 @@ def prelude(name):
     op3=" 3. Take a blunt and smoke a while.\n"
     op4=" 4. Clean your shoes on the grass.\n"
     print(op1+op2+op3+op4)
-    story.append(input("  > "))
-    return story
+    dec = input("  > ")
+    if dec=="1":
+        print ("It worked")
+    elif dec=="2":
+        print ("It worked")
+    elif dec=="3":
+        print ("It worked")
+    elif dec=="4":
+        print ("It worked")
+    else:
+        print("Ah! You seem daring. Oh well, we are going to keep an eye on you then.")
+    
+    
+    return zentrum
 
 
 
 #Main
-name = intro()
-story = prelude(name)
-print(story)
+zentrum = {}
+zentrum = intro(zentrum)
+input("\n             [Press Enter to Continue]")
+zentrum = prelude(zentrum)
+input("\n             [Press Enter to Continue]")
+
+print(zentrum)
